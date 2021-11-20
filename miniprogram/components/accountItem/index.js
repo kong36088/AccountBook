1a1e26a96d47ba9a8a497220a2d0be774121ce31
longz <longz@tencent.com>
2021-11-20 15:31:22 +0800

    stage commit

---
+++ miniprogram/components/accountItem/index.js
@@ -0,0 +1,20 @@
+// miniprogram/components/accountItem/index.js
+const { isMac } = require('../../envList.js');
+
+Component({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    name: "",
+    value: 0,
+  },
+  properties: {
+  },
+  observers: {
+  },
+  methods: {
+  }
+
+});
